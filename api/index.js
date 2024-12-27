57b4a0b0d9740a6eddd408d1eb3ad1df23c971f9
DanSev7 <danielayele077@gmail.com>
2024-12-27 18:49:56 +0300

    Create Signup api route

--- api/index.js
+++ api/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoute.js'
+import authRoutes from './routes/authRoute.js'
 
 
 dotenv.config();
+
 
 mongoose
 .connect(process.env.MONGODB_URL)
@@ -14,10 +16,15 @@ mongoose
     .catch((err)=>{
         console.log(err.message);
     });
-    const app = express();
+
+const app = express();
+
+// Allow to send JSON to the backend 
+app.use(express.json());
     
-    app.listen(process.env.PORT, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
