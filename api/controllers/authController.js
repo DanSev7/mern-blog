fff1dbc5ae8af22b95e2d2e299f437cfaf69b684
DanSev7 <danielayele077@gmail.com>
2025-01-02 23:33:13 +0300

    Edit some formatting

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -7,24 +7,16 @@ export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if(
-        !username || 
-        !email || 
-        !password || 
-        username === '' 
-        || email === '' 
-        || password === ''
+        !username || !email || !password 
+        || 
+        username === '' || email === '' || password === ''
     ) {
         next(errorHandler(400, 'All fields are required'))
     }
     
     const hashedPassword = bcryptjs.hashSync(password, 10);
    
-    const newUser = new User(
-        {
-            username, 
-            email,
-            password: hashedPassword
-        });
+    const newUser = new User({ username, email, password: hashedPassword });
 
         try {
             await newUser.save();
