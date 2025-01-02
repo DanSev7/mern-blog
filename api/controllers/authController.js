3d14edd2cd361b6912afc8f90df9662321c1351b
DanSev7 <danielayele077@gmail.com>
2025-01-02 23:31:48 +0300

    Some Modification

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,8 @@
 import User from '../models/userModel.js'
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
@@ -30,5 +32,39 @@ export const signup = async (req, res, next) => {
         } catch (error) {
             next(error);
         }
+
+}
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password || email === '' || password === '') {
+        next(errorHandler (400, 'All fields are required'));
+    }
+
+    try {
+        const validUser = await User.findOne({ email });
+        if(!validUser) {
+            next(errorHandler(404, 'User not found'));
+        }
+        // bcrypt.js convert the password to hashed then compare it with the Hashed Password
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) {
+            next(errorHandler(400, 'Invalid password'));
+        }
+        
+        const token = jwt.sign(
+            {id:validUser._id},
+            process.env.JWT_SECRET,
+            { expiresIn: '1hr'}
+        )
+        res.status(200).cookie('access_token', token, 
+            { 
+                httpOnly: true,
+            }).json('Signin Successful');
+
+    } catch (error) {
+        next(error);
+    }
 
 }
